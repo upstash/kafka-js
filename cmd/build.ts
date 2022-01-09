eaded92eaa4e96237976ab29d09f9e42a3a27cfe
Andreas Thomas <dev@chronark.com>
2022-01-09 20:04:42 +0100

    fix: polyfill btoa

--- cmd/build.ts
+++ cmd/build.ts
@@ -20,9 +20,16 @@ await build({
           },
         ],
       },
+      {
+        package: { name: "btoa", version: "1.2.1" },
+        globalNames: [{ name: "btoa", exportName: "default" }],
+      },
     ],
   },
   package: {
+    devDependencies: {
+      "@types/btoa": "1.2.3",
+    },
     // package.json properties
     name: "@chronark/upstash-kafka",
     version: Deno.args[0],
