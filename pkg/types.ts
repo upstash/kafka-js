350f0d18852cd06ed47cac1876faada00b4ffcd1
Andreas Thomas <dev@chronark.com>
2022-01-20 23:02:57 +0100

    ci: run always

--- pkg/types.ts
+++ pkg/types.ts
@@ -13,21 +13,21 @@ export type Message = {
   headers: Header[]
 }
 
-export type RetryOptions = {
-  /**
-   * How many retries should be attempted.
-   * For example `retries: 3` will result in a maximum of 4 requests being made.
-   * 1 by default and then 3 retries in case of failure.
-   */
-  retries?: number
+// export type RetryOptions = {
+//   /**
+//    * How many retries should be attempted.
+//    * For example `retries: 3` will result in a maximum of 4 requests being made.
+//    * 1 by default and then 3 retries in case of failure.
+//    */
+//   retries?: number
 
-  /**
-   * Backoff algorithm to use.
-   * Return the time to wait in milliseconds before sending another request
-   *
-   * @default (attempt) => 2 ** attempt * 250
-   *
-   *
-   */
-  backoff?: (attempt: number) => number
-}
+//   /**
+//    * Backoff algorithm to use.
+//    * Return the time to wait in milliseconds before sending another request
+//    *
+//    * @default (attempt) => 2 ** attempt * 250
+//    *
+//    *
+//    */
+//   backoff?: (attempt: number) => number
+// }
